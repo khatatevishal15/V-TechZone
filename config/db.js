2052e118941be8f64dac4ea7f9bb869648fc010c
khatatevishal15 <vishalkhatate2005@gmail.com>
2025-08-03 20:53:19 +0530

    First Commit

--- config/db.js
+++ config/db.js
@@ -1,28 +1,30 @@
 import mongoose from "mongoose";
 
-let cached = global.mongoose
+if (!process.env.MONGODB_URI) {
+  throw new Error("Please define the MONGODB_URI environment variable");
+}
+
+let cached = global.mongoose;
 
 if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
 }
 
 async function connectDB() {
-  if (cached.conn) {
-    return cached.conn
-  }
+  if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
     const opts = {
-        bufferCommands:false
-     }
+      bufferCommands: false,
+    };
 
-     cached.promise = mongoose.connect('${process.env.MONGODB_URI}/v-techzone',opts).then( mongoose => {return mongoose})
-    }
+    cached.promise = mongoose
+      .connect(`${process.env.MONGODB_URI}`, opts) // no extra `/v-techzone` unless needed
+      .then((mongoose) => mongoose);
+  }
 
-    cached.conn = await cached.promise
-    return cached.conn
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
 
-export default connectDB
-
-   
+export default connectDB;
